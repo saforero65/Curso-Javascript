55fef6000e045b384ef3a79ce38c27c3fca4db17
santiago <saforero88@gmail.com>
2021-03-07 08:54:07 -0500

    condicionales

--- 5.Condicionales/scripts.js
+++ 5.Condicionales/scripts.js
@@ -22,8 +22,47 @@ let num = 10;
 
 /* && and */
 /* || o */
-let num1 = 1;
-let num2 = 5;
-if (num1 > 0 && num2 > 0) {
-    console.log(`${num1} y ${num2} son mayores que 0 `);
+let num1 = -10;
+let num2 = -10;
+
+if (num1 > 0) {
+    if (num2 > 0) {
+        console.log(`${num1} y ${num2} son mayores a 0`);
+    } else if (num2 < 0) {
+        console.log(`${num1} es mayor que 0 y ${num2} es menor a 0`);
+    } else {
+        console.log(`${num1} es mayor que 0 y ${num2} es igual a 0`);
+    }
+} else if (num1 < 0) {
+    if (num2 < 0) {
+        console.log(`${num1} y ${num2} son menores a 0`);
+    } else if (num2 > 0) {
+        console.log(`${num1} es mayor que 0 y ${num2} es mayor a 0`);
+    } else {
+        console.log(`${num1} es menor que 0 y ${num2} es igual a 0`);
+    }
+} else {
+    if (num2 < 0) {
+        console.log(`${num1} es igual a 0 y ${num2} es menor a 0`);
+    } else if (num2 > 0) {
+        console.log(`${num1} es igual que 0 y ${num2} es mayor a 0`);
+    } else {
+        console.log(`${num1} es igual que 0 y ${num2} es igual a 0`);
+    }
+}
+
+let word = 'holas';
+if (word.length > 4) {
+    console.log(`word tiene mas de 4 letras`);
+} else if (word.length < 4) {
+    console.log(`word tiene menos de 4 letras`);
+} else {
+    console.log(`word tiene  4 letras`);
+}
+
+let respuesta = !false;
+if (respuesta) {
+    console.log(`Respuesta tiene el valor true`);
+} else {
+    console.log(`Respuesta tiene el valor false`);
 }
