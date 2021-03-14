a75a759b4fffa4f501fc235339ecdde2d1027bf1
Santiago <saforero88@gmail.com>
2021-03-13 22:44:59 -0500

    intro Objetos

---
+++ 15.Objetos/scripts.js
@@ -0,0 +1,14 @@
+const person = {
+        name: 'Juan',
+        age: 26,
+        sons: ['Laura', 'Diego', 'rosa', 'tomas', 'pepe'],
+    }
+    // console.log(person['name'])
+    // for (const key in person) {
+    //     console.log(person[key])
+    // }
+
+// for (const son of person.sons) {
+//     console.log(son)
+// }
+console.log(`Hola ${person.name}. Tienes ${person.age} años y tus hijos se llaman ${person.sons.join(', ')}`)
