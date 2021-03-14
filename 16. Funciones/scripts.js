291792ed58ce3a5c68b880352468682cd4318e7e
santiago <saforero88@gmail.com>
2021-03-14 08:37:57 -0500

    funciones ECMAS6

---
+++ 16. Funciones/scripts.js
@@ -0,0 +1,21 @@
+// function saludar() {
+//     console.log('Hola');
+// }
+// saludar()
+
+// const saludar = () => console.log('Hola')
+// const saludarUsuario = (user) => console.log(`Hola ${user}`)
+// saludarUsuario('Pepe')
+// saludarUsuario('Marta')
+
+// const suma = (num1, num2) => {
+//     if (num1 == 2) {
+//         return num1 + num2
+//     }
+//     return num1
+// }
+// console.log(suma(8, 5))
+const suma = (num1, num2) => num1 + num2
+
+let result = suma(3, 6)
+console.log(result)
