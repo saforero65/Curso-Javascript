ba1d1e3dd01f15fbf0da3315674057b528a20c85
Santiago <saforero88@gmail.com>
2021-06-07 13:18:58 -0500

    manejo de eventos

--- 24.Eventos/scripts.js
+++ 24.Eventos/scripts.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("button");
 const box = document.getElementById("box");
+const input = document.getElementById("input");
 // button.addEventListener("click", () => {
 //     console.log("click");
 // });
@@ -8,9 +9,29 @@ const box = document.getElementById("box");
 //     console.log("Doble CLick");
 // });
 
-box.addEventListener("mouseenter", () => {
-    box.classList.add("green");
+// box.addEventListener("mouseenter", () => {
+//     box.classList.replace("red", "green");
+// });
+// box.addEventListener("mouseleave", () => {
+//     box.classList.replace("green", "red");
+// });
+
+// box.addEventListener("mousedown", () => {
+//     console.log("Has pulsado la caja");
+// });
+// box.addEventListener("mouseup", () => {
+//     console.log("Has soltado el boton  ");
+// });
+
+// box.addEventListener("mousemove", () => {
+//     console.log("estas moviendo el raton en la caja");
+// });
+input.addEventListener("keydown", () => {
+    console.log("Has pulsado una tecla");
 });
-box.addEventListener("mouseleave", () => {
-    box.classList.add("red");
+input.addEventListener("keyup", () => {
+    console.log("Has saltado una tecla");
+});
+input.addEventListener("keypress", () => {
+    console.log("Estas pulsando una tecla");
 });
