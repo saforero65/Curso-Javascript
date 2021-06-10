65c0eac6b6fba8c655662a713ef773b938b8ebda
Santiago <saforero88@gmail.com>
2021-06-10 14:00:36 -0500

    DOM Traversing

---
+++ 27. DOM TRaversing/scripts.js
@@ -0,0 +1,11 @@
+const parent = document.getElementById("parent");
+// PADRES
+// console.log(parent.parentElement);
+// console.log(parent.parentNode);
+// console.log(parent.childNodes);
+// console.log(parent.children[2]);
+// console.log(parent.lastElementChild);
+// console.log(parent.children[0].hasChildNodes());
+// console.log(parent.parentElement.nextElementSibling);
+// console.log(parent.parentElement.previousSibling);
+console.log(parent.parentElement.previousElementSibling);
